f2cc040fa40821c8efcd7713edc6584aca59eb8a
Sean Zhang <sean7218@gmail.com>
2017-08-03 20:33:44 -0400

    First Commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  accessKeyId: 'AKIAIJYOBYG6QZSVF24A',
+  secretAccessKey: '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',
+  region: 'us-east-1'
+}
---
+++ index.js
@@ -0,0 +1,187 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var uuid = require('uuid');
+var aws = require('aws-sdk');
+
+var config = require('./config')
+var s3 = new aws.S3();
+s3.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+  region: 'us-east-1'
+});
+
+var app = express();
+
+var router = express.Router();
+
+router.route('/get_signed_url').get(getSignedURL);
+
+router.route('/listObjects').get(getObjectList);
+
+router.route('/getUrlAsync').get(getUrlAsync);
+
+var keyDescriptions = [
+  {
+    key: "V17492-ICND-0001.pdf",
+    description: "P&ID Legends and Notes"
+  },
+  {
+    key: "V17492-ICND-0002.pdf",
+    description: "Flue Gas P&ID"
+  },
+  {
+    key: "V17492-ICND-0003.pdf",
+    description: "Burner & Ammonia Injection Grid P&ID "
+  },
+  {
+    key: "V17492-ICND-0004.pdf",
+    description: "HP Evaporator & Economizer P&ID"
+  },
+  {
+    key: "V17492-ICND-0005.pdf",
+    description: "HP Superheater P&ID"
+  },
+  {
+    key: "V17492-ICND-0006.pdf",
+    description: "IP Evaporator & Economizer P&ID"
+  },
+  {
+    key: "V17492-ICND-0007.pdf",
+    description: "IP Superheater P&ID"
+  },
+  {
+    key: "V17492-ICND-0008.pdf",
+    description: "LP Economizer P&ID"
+  },
+  {
+    key: "V17492-ICND-0009.pdf",
+    description: "LP Evaporator & Superheater P&ID"
+  },
+  {
+    key: "V17492-ICND-0010.pdf",
+    description: "Reheater P&ID"
+  },
+  {
+    key: "V17492-ICND-0011.pdf",
+    description: "Blowdown Tank P&ID"
+  },
+  {
+    key: "V17492-ICND-0012.pdf",
+    description: "ISilencer Vents and Drains P&ID"
+  },
+  {
+    key: "V17492-ICND-0013.pdf",
+    description: "Process Flow Diagram"
+  },
+  {
+    key: "index.json",
+    description: "Main Document Index"
+  }
+]
+function getSignedURL( req, res, next) {
+
+  var params = {
+    Bucket:'vpi-dcap',
+    Key: uuid.v4(), // set new viersion 4 of uuid which will be the file name
+    Expires: 100,
+    ContentType: 'pdf'
+  };
+
+  s3.getSignedUrl('putObject', params, function(err, signedURL) {
+    console.log("The Params.key")
+    console.log(params.key)
+    if (err) {
+      console.log(err);
+      return next(err);
+    } else {
+      return res.json({
+        postURL: signedURL,
+        getURL: signedURL.split("?")[0]
+      })
+    }
+  })
+}
+
+function getUrlAsync(req, res, next) {
+
+  //Getting all the keys for the buckets
+  var params = {
+    Bucket:'vpi-dcap',
+    Delimiter:'',
+    Prefix:''
+  };
+
+  var bucketObjectKeys;
+  s3.listObjects(params, function(err, data) {
+    if (err) {
+      console.log(err)
+      next(err)
+    } else {
+      bucketObjectKeys = data.Contents.map( a => a['Key'] )
+      //res.json(bucketObjectKeys)
+
+      var objectKey = ''
+      var urlResults = []
+      var url = ''
+      var description = ''
+
+      for (var i = 0; i < bucketObjectKeys.length; i++ ) {
+        objectKey = bucketObjectKeys[i]
+        //objectKey = 'V17492-ICND-0005.pdf'
+        url = s3.getSignedUrl('getObject', { Bucket:'vpi-dcap',   Key: objectKey,  Expires: ( 86400 * 5 )  })
+        description = keyDescriptions.filter( a => a['key'] === objectKey )
+        description = description.length > 0 ? description[0]['description'] : ""
+        urlResults.push({key: objectKey, url: url, description: description});
+      }
+
+      res.json({result:urlResults});
+    }
+  });
+
+
+
+  // objectKey = 'V17492-ICND-0005.pdf'
+  // url = s3.getSignedUrl('getObject', { Bucket:'vpi-dcap',   Key: objectKey })
+  // urlResults.push({key: objectKey, url: url});
+  //
+  // objectKey = "seanbarb.jpg"
+  // url = s3.getSignedUrl('getObject', { Bucket:'vpi-dcap',   Key: objectKey })
+  // urlResults.push({key: objectKey, url: url});
+
+
+  //res.json({result:urlResults});
+
+
+}
+
+function getObjectList( req, res, next) {
+  var params = {
+    Bucket:'vpi-dcap',
+    Delimiter:'',
+    Prefix:''
+  }
+
+  s3.listObjects(params, function(err, data) {
+    if (err) {
+      console.log(err)
+      next(err)
+    } else {
+      console.log(data)
+      var objs = res.json(data)
+      console.log(objs)
+      res.json(data)
+    }
+  })
+}
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use('/v1', router);
+
+var PORT = process.env.PORT || 3000;
+var HOST = process.env.HOST || '127.0.0.1';
+
+console.log("Listening on ", HOST, PORT);
+app.listen(PORT, HOST);
