43600b42820ad667b802e5204f57675ffad1d94e
Sean Zhang <sean7218@gmail.com>
2017-08-11 15:16:51 -0400

    Finished the middleware for parsing index

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var uuid = require('uuid');
-var aws = require('aws-sdk');
-var xlsx = require('xlsx');
-var fs =  require('fs');
-var MongoClient = require('mongodb').MongoClient;
+let express = require('express');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let uuid = require('uuid');
+let aws = require('aws-sdk');
+let xlsx = require('xlsx');
+let fs =  require('fs');
 
-MongoClient.connect('mongodb://localhost:27017/mydb', function(err, db) {
-    if (err) throw err
+let mongoose = require('mongoose');
 
-    db.collection('mycol').find({"result": {}}).toArray(function(err, result) {
+let file = fs.readFileSync('./result.json','utf8');
+let V17494 = JSON.parse(file);
 
-        if (err) throw err
+mongoose.connect('mongodb://localhost/mydb');
 
-        console.log(result)
+let db = mongoose.connection;
+db.once('open', function () {
+        // we are connected
+        //console.log("Connection is established with mydb");
+    });
+let drawingSchema = mongoose.Schema({
+        Key: String,
+        Description: String,
+        Symbol: String
+    }, {
+        collection: 'mycol'
+    });
+let Drawing = mongoose.model('Drawing', drawingSchema);
+Drawing.find({Symbol: 'BCND'}).exec(function (err, result) {
+    if (err) {
+        //console.log(err)
+    } else {
+        //console.log("Successfully returned")
+        //console.log(result)
+    }
+});
 
-    })
 
-})
 
-var config = require('./config');
-var s3 = new aws.S3();
+
+let config = require('./config');
+let s3 = new aws.S3();
 s3.config.update({
     accessKeyId: config.accessKeyId,
     secretAccessKey: config.secretAccessKey,
     region: 'us-east-1'
 });
 
-var app = express();
+let app = express();
 
 
-var router = express.Router();
 
-var keyDescriptions = [
+let router = express.Router();
+
+let keyDescriptions = [
     {
         key: "V17492-ICND-0001.pdf",
         description: "P&ID Legends and Notes"
@@ -90,7 +109,7 @@ var keyDescriptions = [
         key: "index.json",
         description: "Main Document Index"
     }
-]
+];
 
 router.route('/get_signed_url').get(getSignedURL);
 
@@ -100,12 +119,13 @@ router.route('/getUrlAsync').get(getUrlAsync);
 
 router.route('/parseExcel').get(parseExcel);
 
+router.route('/getDrawings').get(getDrawings);
 
 
 function parseExcel(req, res, next) {
-    console.log("Parsing Excel")
-    let workbook = xlsx.readFile("index.xls")
-    let sheet = workbook.Sheets['Export']
+    console.log("Parsing Excel");
+    let workbook = xlsx.readFile("index.xls");
+    let sheet = workbook.Sheets['Export'];
     let result = xlsx.utils.sheet_to_json(sheet);
 
     let docs = result.map( x => {
@@ -120,14 +140,13 @@ function parseExcel(req, res, next) {
 
         return { "Key": key, "Description": description, "Symbol": symbol };
     });
-
     fs.writeFileSync('./result.json', JSON.stringify(docs, null, 4), 'utf-8');
     res.send(docs);
 }
 
 function getSignedURL(req, res, next) {
 
-    var params = {
+    let params = {
         Bucket: 'vpi-dcap',
         Key: uuid.v4(), // set new viersion 4 of uuid which will be the file name
         Expires: 100,
@@ -135,8 +154,8 @@ function getSignedURL(req, res, next) {
     };
 
     s3.getSignedUrl('putObject', params, function (err, signedURL) {
-        console.log("The Params.key")
-        console.log(params.key)
+        console.log("The Params.key");
+        console.log(params.key);
         if (err) {
             console.log(err);
             return next(err);
@@ -151,76 +170,102 @@ function getSignedURL(req, res, next) {
 
 function getUrlAsync(req, res, next) {
 
-    console.log(req);
 
     //Getting all the keys for the buckets
-    var params = {
+    let params = {
         Bucket: 'vpi-dcap',
         Delimiter: '',
         Prefix: ''
     };
 
-    var bucketObjectKeys;
+    let bucketObjectKeys;
     s3.listObjects(params, function (err, data) {
-        if (err) {
-            console.log(err)
-            next(err)
-        } else {
-            bucketObjectKeys = data.Contents.map(a => a['Key'])
+        if (!err) {
+            bucketObjectKeys = data.Contents.map(a => a['Key']
+        );
             //res.json(bucketObjectKeys)
 
-            var objectKey = ''
-            var urlResults = []
-            var url = ''
-            var description = ''
+            let objectKey = '';
+            let urlResults = [];
+            let url = '';
+            let description = '';
+            let symbol = '';
 
-            for (var i = 0; i < bucketObjectKeys.length; i++) {
-                objectKey = bucketObjectKeys[i]
+            for (let i = 0; i < bucketObjectKeys.length; i++) {
+                objectKey = bucketObjectKeys[i];
                 //objectKey = 'V17492-ICND-0005.pdf'
-                url = s3.getSignedUrl('getObject', {Bucket: 'vpi-dcap', Key: objectKey, Expires: ( 86400 * 5 )})
+                url = s3.getSignedUrl('getObject', {Bucket: 'vpi-dcap', Key: objectKey, Expires: ( 86400 * 5 )});
                 description = keyDescriptions.filter(a => a['key'] === objectKey
-            )
-                description = description.length > 0 ? description[0]['description'] : ""
+            );
+                description = description.length > 0 ? description[0]['description'] : "";
                 urlResults.push({key: objectKey, url: url, description: description});
             }
 
             res.json({result: urlResults});
+        } else {
+            console.log(err);
+            next(err)
         }
     });
 
 
-    // objectKey = 'V17492-ICND-0005.pdf'
-    // url = s3.getSignedUrl('getObject', { Bucket:'vpi-dcap',   Key: objectKey })
-    // urlResults.push({key: objectKey, url: url});
-    //
-    // objectKey = "seanbarb.jpg"
-    // url = s3.getSignedUrl('getObject', { Bucket:'vpi-dcap',   Key: objectKey })
-    // urlResults.push({key: objectKey, url: url});
-
-
-    //res.json({result:urlResults});
 
 
 }
 
 function getObjectList(req, res, next) {
-    var params = {
+    let params = {
         Bucket: 'vpi-dcap',
         Delimiter: '',
         Prefix: ''
-    }
+    };
 
     s3.listObjects(params, function (err, data) {
         if (err) {
-            console.log(err)
+            console.log(err);
             next(err)
         } else {
-            console.log(data)
-            var objs = res.json(data)
-            console.log(objs)
+            console.log(data);
+            let objs = res.json(data);
+            console.log(objs);
             res.json(data)
         }
     })
+}
+
+function getDrawings(req, res, next) {
+    let params = {
+        Bucket: 'vpi-dcap',
+        Delimiter: '',
+        Prefix: ''
+    };
+
+    s3.listObjects(params, function(err, data) {
+
+        let dwgKeys = data.Contents.map( a => a['Key']);
+        let dwgDesc = dwgKeys.map( a => {
+            let des = keyDescriptions.filter( d => d.key === a );
+            return {key: a, description: des[0].description} });
+        let dwgSyms = dwgDesc.map( a => {
+            let ke = a.key;
+            let sy = ke.slice(7, 11)
+            return {key: a.key, description: a.description, symbol: sy} });
+        let dwgUrls = dwgSyms.map( a => {
+            let url = s3.getSignedUrl('getObject', {Bucket: 'vpi-dcap', Key: a.key, Expires: ( 86400 * 5 )});
+            return {key: a.key, description: a.description, symbol: a.symbol, url: url}
+        });
+        fs.writeFile('./index.json', JSON.stringify(dwgUrls, null, 2), 'utf8', function (err) {
+            if (err) throw err;
+            console.log("The output.json File has been saved");
+        });
+        res.json(dwgUrls);
+    })
+
+
+}
+
+function uploadIndexJson(req, res, next) {
+    
 }
 
 app.use(morgan('combined'));
@@ -229,8 +274,8 @@ app.use('/v1', router);
 
 
 
-var PORT = process.env.PORT || 3000;
-var HOST = process.env.HOST || '127.0.0.1';
+let PORT = process.env.PORT || 3000;
+let HOST = process.env.HOST || '127.0.0.1';
 
 console.log("Listening on ", HOST, PORT);
 app.listen(PORT, HOST);
